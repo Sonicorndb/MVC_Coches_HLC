e42da39af9ad0c004a40e5c0cf6aeb4501267df8
David Bejarano Llano <sonicorn.db@gmail.com>
2024-12-13 09:22:17 +0100

    Proyecto de Spring Initializer

---
+++ coches_mvc/src/main/java/com/hlc/coches_mvc/CochesMvcApplication.java
@@ -0,0 +1,13 @@
+package com.hlc.coches_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CochesMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CochesMvcApplication.class, args);
+	}
+
+}

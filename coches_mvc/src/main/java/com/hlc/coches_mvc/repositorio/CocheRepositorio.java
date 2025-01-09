c2adb8f2b520b87fbe6c4048b2f1fc774c4a366d
David Bejarano Llano <sonicorn.db@gmail.com>
2025-01-09 09:09:06 +0100

    Cambio a JpaRepository

--- coches_mvc/src/main/java/com/hlc/coches_mvc/repositorio/CocheRepositorio.java
+++ coches_mvc/src/main/java/com/hlc/coches_mvc/repositorio/CocheRepositorio.java
@@ -1,11 +1,12 @@
 package com.hlc.coches_mvc.repositorio;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hlc.coches_mvc.entidad.Coche;
 
 @Repository //indica que esta clase interactúa con los datos (almacenado, búsqueda, modificaciones, eliminaciones...)
-public interface CocheRepositorio extends CrudRepository<Coche, Long>{
+public interface CocheRepositorio extends JpaRepository<Coche, Long>{
 
 }
